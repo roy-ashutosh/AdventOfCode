af67d2a110f164646a874024fc6c9be37d5f0c14
Ashutosh Roy <54039919+roy-ashutosh@users.noreply.github.com>
2024-12-01 12:01:33 +0530

    Day 1 solutions

--- 2024/Sources/Day01/Day01.swift
+++ 2024/Sources/Day01/Day01.swift
@@ -1,11 +1,28 @@
 import AdventOfCode
 
 struct Day01: AdventDay {
-    func part1(input: String) throws -> String {
-        throw AdventError.notImplemented
+    func part1(input: String) throws -> Int {
+        let lines = input.split(separator: "\n").map { line in
+            line.split(separator: " ")
+                .map { Int($0)! }
+        }
+        let left = lines.map { $0[0] }.sorted()
+        let right = lines.map { $0[1] }.sorted()
+        return zip(left, right)
+            .map { abs($0 - $1) }
+            .reduce(0, +)
     }
-
-    func part2(input: String) throws -> String {
-        throw AdventError.notImplemented
+    
+    func part2(input: String) throws -> Int {
+        let lines = input.split(separator: "\n").map { line in
+            line.split(separator: " ")
+                .map { Int($0)! }
+        }
+        let left = lines.map { $0[0] }.sorted()
+        let right = lines.map { $0[1] }.sorted()
+        return left.reduce(0) { sum, leftValue in
+            let matchingCount = right.count(where: { $0 == leftValue })
+            return sum + leftValue * matchingCount
+        }
     }
 }
