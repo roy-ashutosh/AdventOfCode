a1d62a669c7af206e4dea0c05258a1e0204ec93e
Ashutosh <royashutosh1991@gmail.com>
2024-12-12 22:59:17 +0530

    change return type

--- 2024/Sources/Day03/Day03.swift
+++ 2024/Sources/Day03/Day03.swift
@@ -1,11 +1,11 @@
 import AdventOfCode
 
 struct Day03: AdventDay {
-    func part1(input: String) throws -> String {
+    func part1(input: String) throws -> Int {
         throw AdventError.notImplemented
     }
 
-    func part2(input: String) throws -> String {
+    func part2(input: String) throws -> Int {
         throw AdventError.notImplemented
     }
 }
